31f810766a1397a84b6ab283338a049ac241d16c
noveltaker <pnw123456@icloud.com>
2022-07-05 13:42:41 +0900

    first commit

---
+++ src/test/kotlin/com/example/kotlinjpa/KotlinJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
