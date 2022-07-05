9602717de35ad555d1a0ecaa8a139b85ffb013ba
noveltaker <pnw123456@icloud.com>
2022-07-05 15:39:14 +0900

    test: 회원가입 로직 테스트 케이스 추가
    - 중복 회원가입 로직 추가
    - SameEmailException 테스트 케이스 추가

--- src/test/kotlin/com/example/kotlinjpa/service/UserServiceTest.kt
+++ src/test/kotlin/com/example/kotlinjpa/service/UserServiceTest.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinjpa.service
 
+import com.example.kotlinjpa.config.exception.SameEmailException
 import com.example.kotlinjpa.domain.User
 import com.example.kotlinjpa.repository.UserRepository
 import com.example.kotlinjpa.service.dto.UserDTO
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.function.Executable
 import org.mockito.ArgumentMatchers.any
 import org.mockito.BDDMockito
 import org.mockito.Mock
@@ -27,7 +29,7 @@ internal class UserServiceTest {
     }
 
     @Test
-    fun `회원가입 로직`() {
+    fun `회원가입 로직 성공 테스트 케이스`() {
 
         val email = "test@namver.com"
 
@@ -35,15 +37,46 @@ internal class UserServiceTest {
 
         val mock = User(email, password)
 
-        BDDMockito.given(userRepository.save(any())).willReturn(mock)
+        val isExists = false
+
+        BDDMockito.given(userRepository.existsByEmail(email))
+            .willReturn(isExists)
+
+        BDDMockito.given(userRepository.save(any()))
+            .willReturn(mock)
 
         val dto = UserDTO(email, password)
 
         val entity = userService?.signUp(dto)
 
-        BDDMockito.then(userRepository).should(times(1)).save(any())
+        BDDMockito.then(userRepository)
+            .should(times(1))
+            .existsByEmail(email)
+
+        BDDMockito.then(userRepository)
+            .should(times(1))
+            .save(any())
 
         Assertions.assertEquals(mock.getEmail(), entity?.getEmail())
         Assertions.assertEquals(mock.getPassword(), entity?.getPassword())
+    }
+
+    @Test
+    fun `회원 가입 로직 테스트 케이스 SameEmailException`() {
+
+        val email = "test@namver.com"
+
+        val password = "123456"
+
+        val dto = UserDTO(email, password)
+
+        val isExists = true
+
+        BDDMockito.given(userRepository.existsByEmail(email)).willReturn(isExists)
+
+        Assertions.assertThrows(SameEmailException::class.java, Executable {
+            userService?.signUp(dto)
+        })
+
     }
 }
