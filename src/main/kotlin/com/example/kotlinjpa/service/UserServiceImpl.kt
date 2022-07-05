78c86e263a05b05197354a40bc7f2725f7362ab4
noveltaker <pnw123456@icloud.com>
2022-07-05 16:39:37 +0900

    fix: 유저 저장 @Transactional 추가

--- src/main/kotlin/com/example/kotlinjpa/service/UserServiceImpl.kt
+++ src/main/kotlin/com/example/kotlinjpa/service/UserServiceImpl.kt
@@ -5,10 +5,12 @@ import com.example.kotlinjpa.domain.User
 import com.example.kotlinjpa.repository.UserRepository
 import com.example.kotlinjpa.service.dto.UserDTO
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class UserServiceImpl constructor(private val userRepository: UserRepository) : UserService {
 
+    @Transactional
     override fun signUp(dto: UserDTO): User = userRepository.existsByEmail(dto.getEmail()).let {
         if (it) {
             throw SameEmailException()
