fc08ad12d1e09e188f48b55b3d84072019cb981b
noveltaker <pnw123456@icloud.com>
2022-07-12 15:25:50 +0900

    feat: user point column add

--- src/main/kotlin/com/example/kotlinjpa/domain/User.kt
+++ src/main/kotlin/com/example/kotlinjpa/domain/User.kt
@@ -5,19 +5,29 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.PrePersist
 
 @Entity(name = "users")
 data class User constructor(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private var id: Long?,
     @Column(nullable = false) private var email: String?,
-    @Column(nullable = false) private var password: String?
+    @Column(nullable = false) private var password: String?,
+    @Column private var point: Int?
 ) {
 
-    constructor(email: String, password: String) : this(null, email, password)
+    constructor(email: String, password: String) : this(null, email, password, null)
 
     fun getId() = this.id
 
     fun getEmail() = this.email
 
     fun getPassword() = this.password
+
+    @PrePersist
+    fun prePersist() {
+        // user insert 시 null 이면 0 으로 초기값 세팅
+        point.let {
+            it ?: 0
+        }
+    }
 }
