0af9224ea9dc1229fcbb007d65430792bb6d6925
noveltaker <pnw123456@icloud.com>
2022-07-19 20:25:58 +0900

    chore: 오탈자 수정

--- src/main/kotlin/com/example/kotlinjpa/service/EHCacheService.kt
+++ src/main/kotlin/com/example/kotlinjpa/service/EHCacheService.kt
@@ -7,7 +7,7 @@ import java.math.BigDecimal
 @Service
 class EHCacheService {
 
-    @Cacheable(value = ["acheStore"], key = "#number")
+    @Cacheable(value = ["cacheStore"], key = "#number")
     fun getNum(number: Int): BigDecimal {
         val square: BigDecimal = (number as BigDecimal).multiply(number as BigDecimal)
         println("square of $number is $square")
